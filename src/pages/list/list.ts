3190bf52795666ffc7093cfae722fafdb499882c
Tom Gruintjes <tom.gruintjes@gmail.com>
2018-01-11 15:42:49 +0100

    Update for github issue

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -9,28 +9,49 @@ export class ListPage {
   preferredAreas: any;
   allAreas: any;
   displayList: any;
+  myInput: string = "";
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
-    // If we navigated to this page, we will have an item available as a nav param
+
     this.preferredAreas = navParams.get('preferredAreas');
     this.allAreas = navParams.get('allAreas');
 
-    this.refreshDisplayList();
+    /*
+     this.entities.sort(function (a, b) {
+         let x = a['entityname'].toLowerCase();
+         let y = b['entityname'].toLowerCase();
+         return x < y ? -1 : x > y ? 1 : 0;
+       });
+    */
 
+    this.refreshDisplayList();
     console.log('AL AREAS', this.allAreas);
   }
 
   onInput(event) {
-    console.log('EVENT ', event);
+    console.log('EVENT ', event, this.myInput);
+    this.refreshDisplayList();
   }
 
 
   onCancel(event) {
-    console.log('EVENT ', event);
+    console.log('EVENT ', event, this.myInput);
+    this.refreshDisplayList();
   }
 
   refreshDisplayList() {
-    this.displayList = this.preferredAreas.concat(this.allAreas);
+
+    /*
+         this.entities = this.getFilteredEntityList().filter(entity => {
+          return (entity['offtaker'] + ' ' + entity['entityname'] + ' ' + entity['entitydescription']).toLowerCase().indexOf(this.searchTerm.toLowerCase()) > -1
+        })
+    
+        */
+    this.displayList = this.preferredAreas
+      .concat(this.allAreas)
+      .filter(area => {
+        return (area['area_name'] + ' ' + area['city']).toLowerCase().indexOf(this.myInput.toLowerCase()) > -1
+      })
   }
 
   itemTapped(item) {
